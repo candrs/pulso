73469c5c73d70bc9b3dca0344b3983e6a72656de
Andres <andrx_s@hotmail.com>
2017-03-04 11:49:52 -0500

    Clase p

--- formulario.js
+++ formulario.js
@@ -1,22 +1,31 @@
 var $form = $('#formulario'),
     $titulo = $('#titulo'),
-    $ulr = $('#url'),
+    $url = $('#url'),
     $button = $('#mostrar-form'),
     $list = $('#contenido'),
     $post = $('.item').first();
 
+if (localStorage.getItem('autosafe')) {
+  $titulo.val(sessionStorage.getItem('titulo'));
+  $url.val(sessionStorage.getItem('url'));
+}
+
+var id = setInterval(function() {
+  sessionStorage.setItem('titulo', $titulo.val());
+  sessionStorage.setItem('url', $url.val());
+},1000);
 
 function mostrarFormulario() {
   $form.slideToggle();
+  $list.slideToggle();
   return false;
 }
 
 function agregarPost() {
-  console.log('hasta aqui');
-
-  // var url = $url.val(),
   var titulo = $titulo.val(),
+      url = $url.val(),
       $clone = $post.clone();
+
   $clone.find('.titulo_item a')
       .text(titulo)
       .attr('href', url);
@@ -24,7 +33,9 @@ function agregarPost() {
   $clone.hide();
 
   $list.prepend($clone);
-
+  mostrarFormulario();
+  $titulo.val("");
+  $url.val("");
   $clone.fadeIn();
 
   return false;
